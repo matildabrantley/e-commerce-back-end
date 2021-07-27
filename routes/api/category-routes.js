104fafa854819a658f59684c2349c5c2582de4df
Matilda Brantley <matildahbrantley@gmail.com>
2021-07-27 13:57:20 -0400

    Express routes now aynschronous. Todo: add the right Sequelize Model methods inside request

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,29 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  // find all categories, including its associated Products
+  //TODO: use a Sequelize Model's find method (with await)
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  // find one category by its `id` value, including its associated Products
+  //TODO: use one of Sequelize Model's find method (with await)
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  //TODO: use the Sequelize Model create method with await
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  //TODO: use a Sequelize Model put/update method with await
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  //TODO: use a Sequelize Model delete/destroy method with await
 });
 
 module.exports = router;
