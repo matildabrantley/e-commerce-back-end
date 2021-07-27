f03e9a551c5ea81ad91eb2b5910338ec58a77755
Matilda Brantley <matildahbrantley@gmail.com>
2021-07-27 14:08:03 -0400

    All Express routes in tag-routes and product-routes aynschronous. TODO: Add Sequelize Model methods inside each route requests

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,31 +3,29 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
-  
+router.get('/', async (req, res) => {
+  // find all tags, including its associated Product data
+  //TODO: use a Sequelize Model's find method (with await)
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
-
+router.get('/:id', async (req, res) => {
+  // find a single tag by its `id`, including its associated Product data
+  //TODO: use one of Sequelize Model's find method (with await)
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
-
+  //TODO: use the Sequelize Model create method with await
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
-
+  //TODO: use a Sequelize Model put/update method with await
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
-
+  //TODO: use a Sequelize Model delete/destroy method with await
 });
 
 module.exports = router;
